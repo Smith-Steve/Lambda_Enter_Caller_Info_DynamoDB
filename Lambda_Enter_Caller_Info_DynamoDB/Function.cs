c9d9c78ec19755c3596b4c3df25d71574f200143
smith-steve <Steve.A.Dore@outlook.com>
2022-10-15 01:12:51 -0700

    added attributes to tools-defauls.json

--- Lambda_Enter_Caller_Info_DynamoDB/Function.cs
+++ Lambda_Enter_Caller_Info_DynamoDB/Function.cs
@@ -1,4 +1,11 @@
+using Amazon;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using Amazon.DynamoDBv2.DocumentModel;
+using Amazon.DynamoDBv2.Model;
+using Amazon.Runtime;
 using Amazon.Lambda.Core;
+
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
